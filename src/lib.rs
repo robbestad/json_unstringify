254d2c4bc39289b6bc345e1973472d8e24ec4007
Sven Anders Robbestad <1560743+robbestad@users.noreply.github.com>
2022-04-04 10:06:55 +0200

    fmt fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,70 @@
 use regex::Regex;
 use serde_json::{self, Value};
 
-fn remove_dblnewlines(str: &str) -> std::string::String{
+fn remove_newlines(str: &str) -> std::string::String {
     let re = Regex::new(r#"\\n"#).unwrap();
-    let res=re.replace_all(&str, "").to_string();
-    return res;
+    let res = re.replace_all(str, "").to_string();
+    res
 }
-fn remove_slash(str: &str) ->std::string::String{
+fn remove_slash(str: &str) -> std::string::String {
     let re = Regex::new(r#""\\"#).unwrap();
-    let res=re.replace_all(&str, "").to_string();
-    return res;
+    let res = re.replace_all(str, "").to_string();
+    res
 }
-fn remove_doubleap(str: &str) ->std::string::String{
+fn remove_doubleap(str: &str) -> std::string::String {
     let re = Regex::new(r#""""#).unwrap();
-    let res=re.replace_all(&str, "").to_string();
-    return res;
+    let res = re.replace_all(str, "").to_string();
+    res
 }
-fn remove_doubleap2(str: &str) ->std::string::String{
+fn remove_doubleap2(str: &str) -> std::string::String {
     let re = Regex::new(r#""\\""#).unwrap();
-    let res=re.replace_all(&str, "\"").to_string();
-    return res;
+    let res = re.replace_all(str, "\"").to_string();
+    res
 }
 
-fn remove_doubleap3(str: &str) ->std::string::String{
+fn remove_doubleap3(str: &str) -> std::string::String {
     let re = Regex::new(r#"\\""#).unwrap();
-    let res=re.replace_all(&str, "\"").to_string();
-    return res;
+    let res = re.replace_all(str, "\"").to_string();
+    res
 }
 
-fn remove_apstart(str: &str) ->std::string::String{
+fn remove_apstart(str: &str) -> std::string::String {
     let re = Regex::new(r#"\\""#).unwrap();
-    let res=re.replace_all(&str, "\"").to_string();
-    return res;
+    let res = re.replace_all(str, "\"").to_string();
+    res
 }
-fn remove_apend(str: &str) ->std::string::String{
+fn remove_apend(str: &str) -> std::string::String {
     let re = Regex::new(r#"\\}"#).unwrap();
-    let res=re.replace_all(&str, "\"}").to_string();
-    return res;
+    let res = re.replace_all(str, "\"}").to_string();
+    res
 }
-fn remove_brstart(str: &str) ->std::string::String{
+fn remove_brstart(str: &str) -> std::string::String {
     let re = Regex::new(r#"\]"#).unwrap();
-    let res=re.replace_all(&str, "]").to_string();
-    return res;
+    let res = re.replace_all(str, "]").to_string();
+    res
 }
-fn remove_brend(str: &str) ->std::string::String{
+fn remove_brend(str: &str) -> std::string::String {
     let re = Regex::new(r#"\}"]"#).unwrap();
-    let res=re.replace_all(&str, "}]").to_string();
-    return res;
+    let res = re.replace_all(str, "}]").to_string();
+    res
 }
-fn remove_brend2(str: &str) ->std::string::String{
+fn remove_brend2(str: &str) -> std::string::String {
     let re = Regex::new(r#"\\]"#).unwrap();
-    let res=re.replace_all(&str, "\"]").to_string();
-    return res;
+    let res = re.replace_all(str, "\"]").to_string();
+    res
 }
-fn remove_apbr(str: &str) ->std::string::String{
+fn remove_apbr(str: &str) -> std::string::String {
     let re = Regex::new(r#""\{"#).unwrap();
-    let res=re.replace_all(&str, "{").to_string();
-    return res;
+    let res = re.replace_all(str, "{").to_string();
+    res
 }
-fn trimstr(str: &str) ->std::string::String{
+fn trimstr(str: &str) -> std::string::String {
     (&str.trim_end()).to_string()
 }
 
 pub fn parse(input: &str) -> Value {
     let mut temp = input.to_string();
-    temp = remove_dblnewlines(&temp);
-    //temp = remove_newlines(&temp);
+    temp = remove_newlines(&temp);
     temp = remove_slash(&temp);
     temp = remove_doubleap(&temp);
     temp = remove_doubleap2(&temp);
@@ -81,27 +80,26 @@ pub fn parse(input: &str) -> Value {
 
     let json: serde_json::Value =
         serde_json::from_str(&temp[0..temp.len() - 1]).expect("JSON was not well-formatted");
-    //println!("{:?}",json);
-    return json;
+    json
 }
 #[cfg(test)]
 use std::fs::File;
+#[allow(unused_imports)]
 use std::io::Read;
 
 #[test]
-fn parsejson() {
+fn unstringify() {
     let filename = "./log/block3.json";
     match File::open(filename) {
         Ok(mut file) => {
             let mut content = String::new();
             file.read_to_string(&mut content).unwrap();
             let json = parse(&content);
-            assert!(json["blockHeight"]==115724601);
-
-        },
+            assert!(json["blockHeight"] == 115724601);
+        }
         // Error handling.
         Err(error) => {
             println!("Error opening file {}: {}", filename, error);
-        },
+        }
     }
 }
